38ae4978a84c7069ff1b3f0f62054d968a850142
blueana <skrobanastasiia@gmail.com>
2021-08-21 12:54:00 -0500

    merge fixes

--- WebApp/ReactClient/src/components/common/GoogleLoginButton.tsx
+++ WebApp/ReactClient/src/components/common/GoogleLoginButton.tsx
@@ -1,3 +1,4 @@
+import React, { Component } from 'react';
 import { GoogleLogin } from 'react-google-login';
 import AuthenticationService from 'src/services/authenticationService';
 import { notifyError } from 'src/services/notifications/notificationService';
@@ -7,21 +8,23 @@ import { notifyError } from 'src/services/notifications/notificationService';
  class GoogleLoginButton extends Component  {
 
    handleGoogleSingIn = async (event) => {
+
     if (!event.tokenId) {
       notifyError({ msg: 'Unable to get tokenId from Google' });
       return;
     }
-    const result = AuthenticationService.singInByGoogle(event.tokenId);
+   const result = AuthenticationService.singInByGoogle(event.tokenId)
 
-    console.log(result);
+   console.log(result);
   };
 
-  return (
-    <GoogleLogin
-      clientId='306370337343-90vf9prad3vm4ammv78evcm49pdgebd3.apps.googleusercontent.com'
-      buttonText='Sign in with Google'
-      onSuccess={handleGoogleSingIn}
-      onFailure={handleGoogleSingIn}
-    />
-  );
-};
+  render() {
+    return  <GoogleLogin
+    clientId="306370337343-90vf9prad3vm4ammv78evcm49pdgebd3.apps.googleusercontent.com"
+    buttonText="Google Login"
+    onSuccess={this.handleGoogleSingIn}
+    onFailure={this.handleGoogleSingIn}
+  />;
+  }
+}
+export default GoogleLoginButton;
